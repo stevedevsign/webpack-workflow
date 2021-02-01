6b2f67769f20a10c8d754e623f80dd0c32791eaa
stevedevsign <70962798+stevedevsign@users.noreply.github.com>
2021-02-01 17:42:42 -0300

    Update webpack.config.js

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
                     MiniCssExtractPlugin.loader,
-                    'css-loader',
+                    'css-loader?url=false',
                     'sass-loader'
                 ]
             },
